89b3b3f541b6b00ee9c54b63fa3ffbaa4ba4f473
flowerinthenight <chew.esmero@gmail.com>
2017-12-01 04:03:17 +0000

    Use authd-ci user for ecr upload.

--- api.go
+++ api.go
@@ -3,7 +3,13 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/golang/glog"
+	"github.com/mobingilabs/mobingi-sdk-go/pkg/jwt"
 )
+
+type status struct {
+	Status      string `json:"status"`
+	Description string `json:"description"`
+}
 
 type ApiController struct {
 	beego.Controller
@@ -28,10 +34,60 @@ func (c *ApiController) DispatchScratch() {
 }
 
 func (c *ApiController) DispatchRoot() {
-	glog.Info("controller:", c)
-	c.Ctx.ResponseWriter.Write([]byte("root006"))
+	c.Ctx.ResponseWriter.Write([]byte("Copyright (c) Mobingi. All rights reserved."))
 }
 
 func (c *ApiController) DispatchToken() {
-	c.Ctx.ResponseWriter.Write([]byte("Copyright (c) Mobingi. All rights reserved."))
+	ctx, err := jwt.NewCtx()
+	if err != nil {
+		c.Data["json"] = c.serr(err)
+		glog.Error(err)
+		c.ServeJSON()
+		return
+	}
+
+	_ = ctx
+
+	/*
+		var creds credentials
+
+		c.info("body:", string(c.Ctx.Input.RequestBody))
+		err = json.Unmarshal(c.Ctx.Input.RequestBody, &creds)
+		if err != nil {
+			c.Ctx.ResponseWriter.Write(sesha3.NewSimpleError(err).Marshal())
+			c.err(errors.Wrap(err, "unmarshal body failed"))
+			return
+		}
+
+		m := make(map[string]interface{})
+		m["username"] = creds.Username
+		m["password"] = creds.Password
+		tokenobj, stoken, err := ctx.GenerateToken(m)
+		if err != nil {
+			c.Ctx.ResponseWriter.Write(sesha3.NewSimpleError(err).Marshal())
+			c.err(errors.Wrap(err, "generate token failed"))
+			return
+		}
+
+		c.info("token (obj):", tokenobj)
+		c.info("token:", stoken)
+		reply := make(map[string]string)
+		reply["key"] = stoken
+		c.Data["json"] = reply
+		c.ServeJSON()
+	*/
+}
+
+func (c *ApiController) serr(err error) status {
+	return status{
+		Status:      "error",
+		Description: err.Error(),
+	}
+}
+
+func (c *ApiController) sinfo(s string) status {
+	return status{
+		Status:      "success",
+		Description: s,
+	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	goflag "flag"
+	"os"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/golang/glog"
+	"github.com/mobingilabs/mobingi-sdk-go/pkg/private"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
 )
@@ -18,12 +25,16 @@ var (
 		Run:   serve,
 	}
 
-	port string
+	port   string
+	region string
+	bucket string
 )
 
 func init() {
 	rootCmd.Flags().SortFlags = false
-	rootCmd.Flags().StringVar(&port, "port", "8080", "server port")
+	rootCmd.PersistentFlags().StringVar(&port, "port", "8080", "server port")
+	rootCmd.PersistentFlags().StringVar(&region, "aws-region", "ap-northeast-1", "aws region to access aws resources")
+	rootCmd.PersistentFlags().StringVar(&bucket, "token-bucket", "authd", "s3 bucket that contains our public/private pem files")
 	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 }
 
@@ -36,6 +47,12 @@ func serve(cmd *cobra.Command, args []string) {
 	// needed for http input body in request to be available for non-get and head reqs
 	beego.BConfig.CopyRequestBody = true
 
+	err := downloadTokenFiles()
+	if err != nil {
+		err = errors.Wrap(err, "download token files failed, fatal")
+		glog.Exit(err)
+	}
+
 	beego.Router("/", &ApiController{}, "get:DispatchRoot")
 
 	// enable cors
@@ -47,8 +64,54 @@ func serve(cmd *cobra.Command, args []string) {
 		AllowCredentials: true,
 	}))
 
-	glog.Info("start server:", port)
+	glog.Info("start server: ", port)
 	beego.Run(":" + port)
+}
+
+func downloadTokenFiles() error {
+	fnames := []string{"token.pem", "token.pem.pub"}
+	sess := session.Must(session.NewSession())
+	svc := s3.New(sess, &aws.Config{
+		Region: aws.String(region),
+	})
+
+	// create dir if necessary
+	tmpdir := os.TempDir() + "/jwt/rsa/"
+	if !private.Exists(tmpdir) {
+		err := os.MkdirAll(tmpdir, 0700)
+		if err != nil {
+			err = errors.Wrap(err, "mkdir failed: "+tmpdir)
+			glog.Error(err)
+			return err
+		}
+	}
+
+	downloader := s3manager.NewDownloaderWithClient(svc)
+	for _, i := range fnames {
+		fl := tmpdir + i
+		f, err := os.Create(fl)
+		if err != nil {
+			err = errors.Wrap(err, "create file failed: "+fl)
+			glog.Error(err)
+			return err
+		}
+
+		// write the contents of S3 Object to the file
+		n, err := downloader.Download(f, &s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(i),
+		})
+
+		if err != nil {
+			err = errors.Wrap(err, "s3 download failed: "+fl)
+			glog.Error(err)
+			return err
+		}
+
+		glog.Infof("download s3 file: %s (%v bytes)", i, n)
+	}
+
+	return nil
 }
 func main() {
 	err := rootCmd.Execute()
